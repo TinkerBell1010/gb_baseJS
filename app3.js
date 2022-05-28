93bf38c722543506ba3039ddffe77b502baa418f
TinkerBell1010 <68247868+TinkerBell1010@users.noreply.github.com>
2022-05-28 03:54:29 +0300

    hw3

---
+++ app3.js
@@ -0,0 +1,21 @@
+// 3 задание
+
+'use strict';
+
+const products = [{
+        id: 3,
+        price: 200,
+    },
+    {
+        id: 4,
+        price: 900,
+    },
+    {
+        id: 1,
+        price: 1000,
+    },
+];
+
+products.forEach(product => product.priceWithDiscount = product.price * 0.85);
+
+console.log(products);
